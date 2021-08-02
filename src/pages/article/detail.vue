6dd73c0fd90665508ab1eadbe873a44f16efbc0b
李春寅 <lichunyin@jianzhebei.com>
2021-08-02 18:08:04 +0800

    comment

--- src/pages/article/detail.vue
+++ src/pages/article/detail.vue
@@ -10,6 +10,34 @@
 				<text class="createtime-text">日期:{{article.createtime|date('yyyy-mm-dd hh:ss')}}</text>
 			</view>
 		</view>
+		<view class="comment" v-for="(res, index) in commentList" :key="res.id">
+			<view class="left"><image :src="res.url" mode="aspectFill"></image></view>
+			<view class="right">
+				<view class="top">
+					<view class="name">{{ res.name }}</view>
+					<view class="like" :class="{ highlight: res.isLike }">
+						<view class="num">{{ res.likeNum }}</view>
+						<u-icon v-if="!res.isLike" name="thumb-up" :size="30" color="#9a9a9a" @click="getLike(index)"></u-icon>
+						<u-icon v-if="res.isLike" name="thumb-up-fill" :size="30" @click="getLike(index)"></u-icon>
+					</view>
+				</view>
+				<view class="content">{{ res.contentText }}</view>
+				<view class="reply-box">
+					<view class="item" v-for="(item, index) in res.replyList" :key="item.index">
+						<view class="username">{{ item.name }}</view>
+						<view class="text">{{ item.contentStr }}</view>
+					</view>
+					<view class="all-reply" @tap="toAllReply" v-if="res.replyList != undefined">
+						共{{ res.allReply }}条回复
+						<u-icon class="more" name="arrow-right" :size="26"></u-icon>
+					</view>
+				</view>
+				<view class="bottom">
+					{{ res.date }}
+					<view class="reply">回复</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -37,7 +65,84 @@ export default {
 </script>
 
 <style scoped lang="scss">
-	.author-text, createtime-text {
-		padding: 0 10rpx;
+.author-text, createtime-text {
+	padding: 0 10rpx;
+}
+
+.comment {
+	display: flex;
+	padding: 30rpx;
+	.left {
+		image {
+			width: 64rpx;
+			height: 64rpx;
+			border-radius: 50%;
+			background-color: #f2f2f2;
+		}
 	}
+	.right {
+		flex: 1;
+		padding-left: 20rpx;
+		font-size: 30rpx;
+		.top {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			margin-bottom: 10rpx;
+			.name {
+				color: #5677fc;
+			}
+			.like {
+				display: flex;
+				align-items: center;
+				color: #9a9a9a;
+				font-size: 26rpx;
+				.num {
+					margin-right: 4rpx;
+					color: #9a9a9a;
+				}
+			}
+			.highlight {
+				color: #5677fc;
+				.num {
+					color: #5677fc;
+				}
+			}
+		}
+		.content {
+			margin-bottom: 10rpx;
+		}
+		.reply-box {
+			background-color: rgb(242, 242, 242);
+			border-radius: 12rpx;
+			.item {
+				padding: 20rpx;
+				border-bottom: solid 2rpx $u-border-color;
+				.username {
+					font-size: 24rpx;
+					color: #999999;
+				}
+			}
+			.all-reply {
+				padding: 20rpx;
+				display: flex;
+				color: #5677fc;
+				align-items: center;
+				.more {
+					margin-left: 6rpx;
+				}
+			}
+		}
+		.bottom {
+			margin-top: 20rpx;
+			display: flex;
+			font-size: 24rpx;
+			color: #9a9a9a;
+			.reply {
+				color: #5677fc;
+				margin-left: 10rpx;
+			}
+		}
+	}
+}
 </style>
