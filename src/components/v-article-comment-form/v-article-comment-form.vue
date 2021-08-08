5e7973ca2cb211015d6b6bb5eb375a4506b1cb51
lichunyin <licy2013@aliyun.com>
2021-08-08 22:36:15 +0800

    comment

---
+++ src/components/v-article-comment-form/v-article-comment-form.vue
@@ -0,0 +1,87 @@
+<template>
+  <u-popup v-model="show" mode="bottom" @close="$emit('input', show)" safe-area-inset-bottom>
+    <view class="wrap">
+      <u-form ref="commentForm" :model="comment_form">
+        <u-form-item class="reply-input" prop="content">
+          <u-input type="textarea" v-model="comment_form.content" maxlength="65535" border />
+          <view slot="right">
+            <u-button type="primary" plain @tap="doSendComment()" >发送</u-button>
+          </view>
+        </u-form-item>
+      </u-form>
+    </view>
+  </u-popup>
+</template>
+
+<script>
+import { createApi } from '@/api/article-comment'
+
+const init_comment_form = {
+	article_seq: 0,
+	parent_seq: 0,
+	content: ''
+}
+export default {
+  name: 'v-article-comment-form',
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    },
+    article_seq: {
+      type: String,
+      default: "0"
+    },
+    parent_seq: {
+      type: String,
+      default: "0"
+    }
+  },
+	watch: {
+			value(nVal, oVal) {
+        console.log('watch value', nVal, oVal)
+				this.show = nVal
+			}
+		},
+	data() {
+		return {
+			show: false,
+			comment_form: Object.assign({}, init_comment_form),
+			rules: {
+				content: [{
+					required: true,
+					type: 'string',
+					message: '请输入评论内容'
+				}]
+			}
+		}
+	},
+	methods:{
+		validate(){
+			let verify = false
+			this.$refs.commentForm.setRules(this.rules)
+			this.$refs.commentForm.validate(valid => {
+				verify = valid
+			})
+			return verify
+		},
+		doSendComment() {
+			if(this.validate() == false){
+				return false
+			}
+      this.comment_form.article_seq = this.article_seq
+      this.comment_form.parent_seq = this.parent_seq
+			createApi(this.comment_form).then(res=>{
+				console.log(res)
+				this.show_comment_form = false
+				this.comment_form	= Object.assign({}, init_comment_form)
+				this.$u.toast('评论成功')
+			})
+		}
+	}
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
